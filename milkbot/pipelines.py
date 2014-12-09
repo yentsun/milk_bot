ff7aaafe488da121f84502ff99722f91b2450cb8
Max Korinets <mkorinets@gmail.com>
2014-12-09 12:32:34 +0300

    post all reports in one request

--- milkbot/pipelines.py
+++ milkbot/pipelines.py
@@ -1,53 +1,30 @@
 # -*- coding: utf-8 -*-
 
-import transaction
-import logging
-from ZODB.FileStorage import FileStorage
-from scrapy.exceptions import DropItem
-from milkpricereport.models import (PriceReport, Product, ProductCategory,
-                                    Reporter, Merchant, StorageManager,
-                                    CategoryLookupError, PackageLookupError)
-
-logging.basicConfig(filename='dropped.log', level=logging.DEBUG)
+import requests
 
 
 class PersistencePipeline(object):
     """Pipeline to persist items in ZODB"""
 
     def __init__(self):
-        self.keeper = StorageManager(FileStorage('storage.fs'))
-        self.errors = list()
-        self.success = list()
+        self.payload = list()
 
     def process_item(self, item, spider):
 
-        merchant = Merchant.fetch(item['merchant'], self.keeper)
-        reporter = Reporter.acquire(spider.name, self.keeper)
-        try:
-            report, stats = PriceReport.assemble(
-                price_value=float(item['price_value']),
-                product_title=item['title'],
-                url=item['url'],
-                merchant=merchant,
-                reporter=reporter,
-                storage_manager=self.keeper
-            )
-            self.success.append(report)
-        except CategoryLookupError, e:
-            logging.debug(e.message.encode('utf-8'))
-            self.errors.append(e)
-            raise DropItem(e.message)
-        except PackageLookupError, e:
-            logging.debug(e.message.encode('utf-8'))
-            self.errors.append(e)
-            raise DropItem(e.message.encode('utf-8'))
+        self.payload.append(('url', item['url']))
+        self.payload.append(('price_value', item['price_value']))
+        self.payload.append(('product_title', item['title']))
+        self.payload.append(('merchant_title', item['merchant']))
+        self.payload.append(('reporter_name', spider.name))
 
         return item
 
     def close_spider(self, spider):
 
-        if len(self.success) > 0:
-            transaction.commit()
-        else:
-            transaction.abort()
-        self.keeper.close()
+        response = requests.post('http://localhost:6543/reports',
+                                 data=self.payload)
+        if response.status_code == 200:
+            print(response.json())
+        if response.status_code == 500:
+            print(response.text)
+            raise Exception('Server error!')
